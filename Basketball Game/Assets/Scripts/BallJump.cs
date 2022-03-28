b93eaca4aa15a549ae7a772a9f9898741cfba8ad
AMatti12 <100966409+AMatti12@users.noreply.github.com>
2022-03-28 13:15:17 -0600

    Ball jumping function added

---
+++ Basketball Game/Assets/Scripts/BallJump.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallJump : MonoBehaviour
+{
+    private BallJump_Base ballBase;
+    private RigidBody2D rigidBody2D
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        ballBase = gameObject.GetComponent<BallJump_Base>();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            float jumpVelocity = 100f;
+            rigidBody2D.velocity = Vector2.ip * jumpVelocity
+        }
+    }
+}
