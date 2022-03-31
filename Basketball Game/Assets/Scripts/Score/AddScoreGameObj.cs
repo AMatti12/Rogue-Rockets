add486032222718aff2ea07e1e7f9f80823a1626
AMatti12 <100966409+AMatti12@users.noreply.github.com>
2022-03-31 14:38:31 -0600

    Done for the day

--- Basketball Game/Assets/Scripts/Score/AddScoreGameObj.cs
+++ Basketball Game/Assets/Scripts/Score/AddScoreGameObj.cs
@@ -1,11 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class AddScoreGameObj : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D collision)
+    //ScoreAdder scoreX = FindObjectOfType<ScoreAdder>();
+    //public static Object[] FindObjectsOfType(Type type);
+   public static List<ScoreAdder> scoreX = FindObjectsOfType<ScoreAdder>();
+
+
+private void Update()
+{
+    Debug.Log(scoreX.Length());
+}
+private void OnTriggerEnter2D(Collider2D collision)
     {
-        ScoreAdder.score++;
+        Debug.Log(scoreX.currentScore);
+        //scoreX.scoreText.text = scoreX.currentScore.ToString();
     }
+
 }
