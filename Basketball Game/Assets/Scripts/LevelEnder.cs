90620f7674998075d30039f21652aa4930307115
AMatti12 <100966409+AMatti12@users.noreply.github.com>
2022-03-31 12:58:19 -0600

    Box Collider issue fixed

--- Basketball Game/Assets/Scripts/LevelEnder.cs
+++ Basketball Game/Assets/Scripts/LevelEnder.cs
@@ -6,11 +6,13 @@ public class LevelEnder : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        
         SceneLoader sceneloader = FindObjectOfType<SceneLoader>();
-
+        Debug.Log(sceneloader);
         if (tag == "Discontinuity")
         {
             sceneloader.LoadNextScene();
         }
     }
+
 }
