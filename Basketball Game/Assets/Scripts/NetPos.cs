28ba07964a064550b7743d8813a91f3551e597bb
AMatti12 <100966409+AMatti12@users.noreply.github.com>
2022-04-03 13:26:19 -0600

    Net bug

--- Basketball Game/Assets/Scripts/NetPos.cs
+++ Basketball Game/Assets/Scripts/NetPos.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class NetPos : MonoBehaviour
 {
+    //float panelWidth = menuCanvasRectTransform.rect.width * menuCanvasRectTransform.localScale.x;
+    //float panelHeight = menuCanvasRectTransform.rect.height * menuCanvasRectTransform.localScale.y;
     // Start is called before the first frame update
     void Start()
     {
-        Vector2 netPos = new Vector2(transform.position.x, Random.Range(8f, 2f));
+        Vector2 netPos = new Vector2(transform.position.x, Random.Range(690f, 545f));
         transform.position = netPos;
+
+        /*
+        Canvas canvas = FindObjectOfType<Canvas>();
+        float h = canvas.GetComponent<RectTransform>().rect.height;
+        Debug.Log(h);*/
     }
 
 }
