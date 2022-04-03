fca9e9b29270ff7820970a4d4bf7c149704e8c85
AMatti12 <100966409+AMatti12@users.noreply.github.com>
2022-04-03 09:49:48 -0600

    Scoring system repaired

---
+++ Basketball Game/Assets/Scripts/Score/ScoreSystem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreSystem : MonoBehaviour
+{
+    [SerializeField] int currentScore = 0;
+    [SerializeField] int pointsPerTrigger = 5;
+    [SerializeField] TextMeshProUGUI scoreText;
+
+    private void Start()
+    {
+        scoreText.text = currentScore.ToString();
+    }
+
+    public void AddToScore()
+    {
+        currentScore += pointsPerTrigger;
+        scoreText.text = currentScore.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
