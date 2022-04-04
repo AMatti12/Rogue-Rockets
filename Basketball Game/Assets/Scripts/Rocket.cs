99ed87446e859e962ced5cded4181fd2c29d15be
AMatti12 <100966409+AMatti12@users.noreply.github.com>
2022-04-04 12:41:12 -0600

    Effects fixed

---
+++ Basketball Game/Assets/Scripts/Rocket.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    [SerializeField] GameObject scoreSparklesVFX;
+    [SerializeField] GameObject boostSparklesVFX;
+    
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log("Here");
+    if (col.tag == "BoostCompatible")
+    {
+        var effectPosition1 = col.transform.position;
+        //effectPosition.transform.position = new Vector3(effectPosition.x, effectPosition.y, -4.0f);
+        //effectPosition.position.z = -4.0f;
+        Debug.Log(effectPosition1);
+        //col.transform.position.z = -4;
+        //Debug.Log(transform.position);
+        GameObject boost = Instantiate(boostSparklesVFX, effectPosition1, transform.rotation);
+        Destroy(col.gameObject);
+    }
+    else if (col.tag == "ScoreCompatible")
+    {
+        var effectPosition2 = col.transform.position;
+        GameObject coin = Instantiate(scoreSparklesVFX, effectPosition2, transform.rotation);
+        Destroy(col.gameObject);
+    }
+    }
+
+}
